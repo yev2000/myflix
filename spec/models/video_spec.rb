423eec13431ec9e4eea6d03138303c3fdb980e88
Yev Gurevich <yev2000@verizon.net>
2014-10-14 14:12:13 -0400

    used before action to seed database - cleans up the tests.  Also broke up one test into two having to do with searching for items with spaces in their names

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,30 +6,36 @@ def create_base_video_set
 end
 
 describe Video do
+  before do
+    create_base_video_set
+  end
 
   # this is using the shoulda notation
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
   it { should have_many(:categories).order("name") }
 
+  #########################################
+  #
+  # Tests for the Video title search
+  #
+  #########################################
   describe "#search_by_title" do
     it "returns empty array for no matches" do
-      # seed videos with a reference set of videos
-      create_base_video_set
       expect(Video.search_by_title("Godfather")).to eq([])
+    end
+
+    it "returns empty array for no matches on string with spaces where each word is individually a match" do
+      # seed videos with a reference set of videos
       expect(Video.search_by_title("Green October")).to eq([])
       expect(Video.search_by_title("green october")).to eq([])
     end
 
     it "returns a single match for a single exact match" do
-      create_base_video_set
-
       expect(Video.search_by_title("Forrest Gump")).to eq([Video.find_by(title: "Forrest Gump")])
     end
 
     it "returns an array of matches for a multiple match" do
-      create_base_video_set
-
       expect(Video.search_by_title("The")).to match_array(
         [
           Video.find_by(title: "The Hunt for Red October"),
@@ -39,7 +45,6 @@ describe Video do
     end
 
     it "returns matches case-insensitive" do
-      create_base_video_set
       expect(Video.search_by_title("the")).to match_array(
         [
           Video.find_by(title: "The Hunt for Red October"),
