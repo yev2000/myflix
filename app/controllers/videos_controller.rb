b86be17883e445becfe1deb08283b0064418525f
Yev Gurevich <yev2000@verizon.net>
2014-10-20 15:43:17 -0400

    some refactoring, adding of additional tests, and firming up with respect to solution

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -35,16 +35,18 @@ class VideosController < ApplicationController
       flash[:danger] = "There is no video with ID #{params[:id]}.  Showing all videos instead."
       redirect_to videos_path
     else
+      # the video being reviewed does exist.
+
       @review = Review.new(review_params)
       @review.video = @video
       @review.user = current_user_get
-      binding.pry
-
+  
       if (@review.save)
         flash[:success] = "Your review for #{@video.title} has been recorded"
         redirect_to video_path(@video)
       else
         # error messages should already be set on the @review instance
+        # if validations have failed.
         render :show
       end
     end
