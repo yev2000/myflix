15d49a56ecef69166bfd23c4444de3045cfb4c69
Yev Gurevich <yev2000@verizon.net>
2014-11-07 23:11:48 -0500

    added following model and db migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,9 @@ class User < ActiveRecord::Base
   has_many :sorted_video_queue_entries, -> { order("position") }, class_name: "VideoQueueEntry"
   has_many :queued_videos, -> { order("position") }, {through: :video_queue_entries, source: :video}
 
+  has_many :followings
+  has_many :followed_users, through: :followings
+
   validates :email, presence: true, uniqueness: true, length: {minimum: 3}
   validates :password, presence: true, on: :create, length: {minimum: 4}
   validates :fullname, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141020204805) do
+ActiveRecord::Schema.define(version: 20141107225035) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "followings", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "followed_user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
