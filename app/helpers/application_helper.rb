c588e3aeda87d55d6f8504d1ccb364dd57c92e53
Yev Gurevich <yev2000@verizon.net>
2014-11-19 01:10:58 -0500

    Reved version by one dot-dot

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-  APP_VERSION = "0.2"
+  APP_VERSION = "0.2.1"
 
   def myflix_app_version
     build_date_string = ENV['MYFLIX_BUILD_DATE'] ? " -- " + ENV['MYFLIX_BUILD_DATE'] : "  Development or Test build"
