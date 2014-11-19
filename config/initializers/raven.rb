88c0866de8e269920f97e20dd03034055d111073
Yev Gurevich <yev2000@verizon.net>
2014-11-18 23:47:44 -0500

    Removed intentional error that I used to test sentry from Heroku; made raven only active in non-test environments since I don't need Sentry error notifications in test

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,7 @@
-require 'raven'
+if !Rails.env.test?
+  require 'raven'
 
-Raven.configure do |config|
-  config.dsn = ENV['SENTRY_DSN']
+  Raven.configure do |config|
+    config.dsn = ENV['SENTRY_DSN']
+  end
 end
