02466072591a5d9bc87346430fd075f8ee70790c
Yev Gurevich <yev2000@verizon.net>
2014-11-21 20:12:32 -0500

    fixed bug in migration code where wrong name of video columns was being used in drops

--- db/migrate/20141121051757_video_cover_fields_rename_and_delete.rb
+++ db/migrate/20141121051757_video_cover_fields_rename_and_delete.rb
@@ -2,8 +2,8 @@ class VideoCoverFieldsRenameAndDelete < ActiveRecord::Migration
   def change
     add_column :videos, :large_cover, :string
     add_column :videos, :small_cover, :string
-    remove_column :videos, :large_cover_url
-    remove_column :videos, :small_cover_url
+    remove_column :videos, :cover_small_url
+    remove_column :videos, :cover_large_url
     remove_column :videos, :cover
   end
 end
